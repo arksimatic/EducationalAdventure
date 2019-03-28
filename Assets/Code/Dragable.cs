8a602cf1f96203d638cb02f0bd42390ec186854c
Madoxen <pchlad5@gmail.com>
2019-03-28 21:16:00 +0100

    Basic drag and drop implemented, now working on android touch controls

---
+++ Assets/Code/Dragable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System;
+
+
+[RequireComponent(typeof(EventTrigger))]
+public class Dragable : MonoBehaviour
+{
+    void Start()
+    {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.Drag;
+        entry.callback.AddListener((data) => { OnDrag((PointerEventData)data); });
+        trigger.triggers.Add(entry);
+
+
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        #if UNITY_EDITOR
+        transform.position = Input.mousePosition;
+        #endif
+
+    }
+
+
+
+}
