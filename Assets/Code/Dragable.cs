f2eb307bce3105de7ecb87e5bf944a7b6d6a12fa
Madoxen <pchlad5@gmail.com>
2019-03-29 14:29:34 +0100

    Added code for android, untested

--- Assets/Code/Dragable.cs
+++ Assets/Code/Dragable.cs
@@ -25,10 +25,21 @@ public class Dragable : MonoBehaviour
     public void OnDrag(PointerEventData data)
     {
         #if UNITY_EDITOR
+        //Since we operate on UI, we can simply set position to screen touch coords
         transform.position = Input.mousePosition;
+        #elif UNITY_ANDROID
+        //Sanity check
+        if(Input.touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            //Since we operate on UI, we can simply set position to screen touch coords
+            transform.position = t.position;
+        }
         #endif
 
+
     }
+
 
 
 
