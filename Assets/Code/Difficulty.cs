a96f0ebdf43c78829dea05416583c665e6a09217
Madoxen <pchlad5@gmail.com>
2019-04-02 14:57:58 +0200

    added empty classes

---
+++ Assets/Code/Difficulty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+//Class describing difficulty setting
+[CreateAssetMenu(fileName = "difficulty", menuName = "Game Data/DifficultySetting")]
+public class Difficulty : ScriptableObject
+{
+    public string name = "";
+    public List<SceneCollection> minigames = new List<SceneCollection>();
+}
+
+
---
+++ Assets/Code/SceneCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+[Serializable]
+public class SceneCollection
+{
+    public List<Scene> scenes = new List<Scene>();
+}
+
