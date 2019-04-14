a6f6254b365412abfba23d13a010a9ffa8e9414c
Madoxen <pchlad5@gmail.com>
2019-04-14 15:58:18 +0200

    Fixed FillBlankMinigame scene hierarchy; added difficulty settings for FillBlankMinigame - Awaiting difficulty content files; fixed layout in FillBlankMinigame; fixed files not loading properly in FillBlankMinigame on Android builds

--- Assets/Code/FillBlankMinigame/SettingPhrases.cs
+++ Assets/Code/FillBlankMinigame/SettingPhrases.cs
@@ -11,29 +11,24 @@ public class SettingPhrases : MonoBehaviour
     public static string[] m = new string[150];
     public static string word = null;
     public static string txt = null;
+
+
+    //TODO: Add new difficulty levels
+    public static readonly string[] difficulties = new string[] { "klasa1", "klasa1", "klasa1" };
+
+
+
     public static void Draw(Text Word)
     {
-        string path = "Assets/Resources/klasa1.txt";
-        StreamReader reader = new StreamReader(path);
-        txt = File.ReadAllText(path);
+        string text = Resources.Load<TextAsset>(difficulties[(int)Difficulty.diff]).text;
+        StringReader reader = new StringReader(text);
         reader.Close();
 
-        /*Word.text = System.String.Format(
-            "Po t{0}m ciem{1}ym b{2}ru \r\n" +
-            "Kuku{3}e{4}ka kuka, \r\n" +
-            "Z ran{5} a do wie{6}ora \r\n" +
-            "Gniaz{7}ka sobi{8} sz{9}ka. \r\n" +
-            "Ku{10}u!K{11}ku! \r\n" +
-            "Gnia{12}dka so{13}ie szu{14}a. ",
-            j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8], j[9], j[10], j[11], j[12], j[13], j[14]);*/
-
-        char[] charArr = txt.ToCharArray();
+        char[] charArr = text.ToCharArray();
         word = null;
         int h = 1;
         foreach (char ch in charArr)
         {
-            //if(ch=='/')
-                //{ word += System.String.Join("", "\r\n"); }
             if(ch=='_')
             {
                 word = System.String.Format(word + j[h]);
@@ -54,12 +49,13 @@ public class SettingPhrases : MonoBehaviour
 
     void Start()
     {
-        //!File.Exists(textFile)
+
+
+        string anwsers = Resources.Load<TextAsset>(difficulties[(int)Difficulty.diff] + "odp").text;
 
 
 
-        string pathodp = "Assets/Resources/klasa1odp.txt";
-        StreamReader readerodp = new StreamReader(pathodp);
+        StringReader readerodp = new StringReader(anwsers);
         m[0] = "a";
         for (int i = 1; m[i-1] != null; i++)
         {
